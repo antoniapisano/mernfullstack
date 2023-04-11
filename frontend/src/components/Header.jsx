6f7893a5cc32c3f8e2cf8a46577d2948e6a350cc
antoniapisano <antoniapisano@campus.masterschool.com>
2023-04-11 19:57:36 +0100

    frontendsetup ctd

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
+import { Link, useNavigate } from 'react-router-dom'
+
+function Header() {
+  return (
+    <div>header</div>
+  )
+}
+
+export default Header
