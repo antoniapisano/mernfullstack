525e44515c99902d77b9a5cea3468d3c14b3bd03
antoniapisano <antoniapisano@campus.masterschool.com>
2023-04-17 23:32:45 +0100

    started form frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>header</div>
+    <header className='header'>
+      <div className='logo'>
+        <Link to='/'>GoalSetter</Link>
+      </div>
+      <ul>
+          <li>
+            <button className='btn'>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+          <>
+            <li>
+              <Link to='/login'>
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to='/register'>
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+      </ul>
+    </header>
   )
 }
 
