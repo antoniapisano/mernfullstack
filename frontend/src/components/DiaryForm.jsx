6502a52ceccd32511ef3e3c256ba9a15e10e9a19
antoniapisano <antoniapisano@campus.masterschool.com>
2023-05-19 14:55:57 +0100

    entries autofill at same time

--- frontend/src/components/DiaryForm.jsx
+++ frontend/src/components/DiaryForm.jsx
@@ -22,8 +22,57 @@ function DiaryForm() {
           <label htmlFor='text'>Diary</label>
           <input
             type='text'
-            name='text'
-            id='text'
+            name='name_of_wine'
+            id='name_of_wine'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type='text'
+            name='vintage'
+            id='vintage
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type="text"
+            name="price"
+            id="price'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+           <input
+            type='text'
+            name='place_of_purchase'
+            id='place_of_purchase'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type='text'
+            name='primary_aromas'
+            id='primary_aromas'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type='text'
+            name='secondary_aromas'
+            id='secondary_aromas'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type='text'
+            name='tertiary_aromas'
+            id='tertiary_aromas'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type='text'
+            name='description'
+            id='description'
             value={text}
             onChange={(e) => setText(e.target.value)}
           />
